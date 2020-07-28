ca3037435ba027cd7640d5ee05fb439bc9dab48b
CHUYC <zuwtz31292@@gmail.com>
2020-07-28 12:19:11 +0800

    upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# LineBot-python-NKUST" 
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+app = Flask(__name__)
+# Channel Access Token
+line_bot_api = LineBotApi('Channel access token')
+# Channel Secret
+handler = WebhookHandler('Channel secret')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def pretty_echo(event):
+    text = event.message.text
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+
+        if text == 'test':
+            text='success'
+        else:
+            text=text    
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=text)
+        )
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
